ccfb6e2d858adf7ab12680e67d3cc87ceb8accb9
Diece98 <esther.mehal@gmail.com>
2024-03-22 16:41:47 +0100

    little front

--- app/Dashboard/layout.jsx
+++ app/Dashboard/layout.jsx
@@ -1,73 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
-import styles from "@/app/ui/dashboard/products/products.module.css";
-import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Navbar from "../ui/dashboard/navbar/navbar"
+import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import styles from "../ui/dashboard/dashboard.module.css"
+import Footer from "../ui/dashboard/footer/footer"
 
-
-const ProductsPage = async ({ searchParams }) => {
-    const q = searchParams?.q || "";
-    const page = searchParams?.page || 1;
-    const { count, products } = await fetchProducts(q, page);
-
+const Layout = ({ children }) => {
     return (
         <div className={styles.container}>
-            <div className={styles.top}>
-                <Link href="/dashboard/skills">
-                    <button className={styles.addButton}>Add New</button>
-                </Link>
+            <div className={styles.menu}>
+                <Sidebar />
             </div>
-            <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <td>Title</td>
-                        <td>Description</td>
-                        <td>Price</td>
-                        <td>Created At</td>
-                        <td>Stock</td>
-                        <td>Action</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product) => (
-                        <tr key={product.id}>
-                            <td>
-                                <div className={styles.product}>
-                                    <Image
-                                        src={product.img || "/noproduct.jpg"}
-                                        alt=""
-                                        width={40}
-                                        height={40}
-                                        className={styles.productImage}
-                                    />
-                                    {product.title}
-                                </div>
-                            </td>
-                            <td>{product.desc}</td>
-                            <td>${product.price}</td>
-                            <td>{product.createdAt?.toString().slice(4, 16)}</td>
-                            <td>{product.stock}</td>
-                            <td>
-                                <div className={styles.buttons}>
-                                    <Link href={`/dashboard/products/${product.id}`}>
-                                        <button className={`${styles.button} ${styles.view}`}>
-                                            View
-                                        </button>
-                                    </Link>
-                                    <form action={deleteProduct}>
-                                        <input type="hidden" name="id" value={product.id} />
-                                        <button className={`${styles.button} ${styles.delete}`}>
-                                            Delete
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <Pagination count={count} />
+            <div className={styles.content}>
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default ProductsPage;
+export default Layout
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+// import Experience from "@/components/Experience";
+// import About from "@/components/About";
+// import Projects from "@/components/Projects";
+// import Skills from "@/components/Skills";
+import Image from "next/image";
 
-const Homepage = () => {
+export default function Home() {
   return (
-    <div>Homepage</div>
-  )
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        {/* <About />
+        <Skills />
+        <Experience />
+        <Projects /> */}
+      </div>
+    </main>
+  );
 }
-
-export default Homepage
