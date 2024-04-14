aa10362400e19e619819057b65b494b458b5607c
Diece98 <esther.mehal@gmail.com>
2024-04-14 20:06:25 +0200

    redoing front and linking page cleanly

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './ui/globals.css'
-import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
@@ -17,7 +16,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/instagram.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/discord.svg",
+    },
+  ];
+
+  
+  
+  
+  
